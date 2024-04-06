8d5f0a59bba5bed839b4a74ec5064920a5272eca
Chintamani Vijaykumar Wankhede <125362482+chintamani07@users.noreply.github.com>
2024-04-06 14:06:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result  ;
+
+            Console.WriteLine("WELCOME TO CALCULATOR");
+
+            Console.WriteLine("Please Enter The Value of First Number : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value of Second Number : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
